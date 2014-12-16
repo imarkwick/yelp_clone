fa723217630462695d43e2e5648da28dae76b33e
imarkwick <izzymarkwick@gmail.com>
2014-12-16 17:47:23 +0000

    can delete a restaurant

--- spec/features/restaurants_features_spec.rb
+++ spec/features/restaurants_features_spec.rb
@@ -58,4 +58,16 @@ feature 'restaurants' do
 	   expect(current_path).to eq '/restaurants'
 	  end
 	end
+
+	describe 'deleting restaurants' do
+
+  	before {Restaurant.create name: 'KFC'}
+
+  	scenario 'removes a restaurant when a user clicks a delete link' do
+    	visit '/restaurants'
+    	click_link 'Delete KFC'
+    	expect(page).not_to have_content 'KFC'
+	    expect(page).to have_content 'Restaurant deleted successfully'
+  	end
+	end
 end
