ad8bd62938a8e794ee5741d4dd70768150343866
imarkwick <izzymarkwick@gmail.com>
2014-12-17 14:08:56 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  get 'restaurants' => 'restaurants#index'
+  root to: 'restaurants#index' 
 
   resources :restaurants do
     resources :reviews
